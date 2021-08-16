fe4a060236b8d35cabeb71f5f0e6bfc031ac2a7a
Bruno Konrad <bruno.skon@gmail.com>
2021-08-16 15:36:00 +0200

    Fix projectile on screen calculation

--- src/code/systems/ProjectileMovement.ts
+++ src/code/systems/ProjectileMovement.ts
@@ -1,6 +1,7 @@
 import { canvas } from "../Canvas";
 import { Direction } from "../components/Direction";
 import { Position } from "../components/Position";
+import { Size } from "../components/Size";
 import { Velocity } from "../components/Velocity";
 import { Entities } from "../Entities";
 import { Entity } from "../entities/Base";
@@ -9,13 +10,14 @@ import { System } from "./Base";
 
 export class ProjectileMovement extends System {
   constructor() {
-    super(["Projectile", "Direction", "Position", "Velocity"]);
+    super(["Projectile", "Direction", "Position", "Size", "Velocity"]);
   }
 
   execute(entity: Entity, dt: number) {
     const position = entity.component<Position>("Position");
     const direction = entity.component<Direction>("Direction");
     const velocity = entity.component<Velocity>("Velocity");
+    const size = entity.component<Size>("Size");
 
     const change = vec2(
       dt * direction.value.x * velocity.value,
@@ -25,8 +27,10 @@ export class ProjectileMovement extends System {
     position.x += change.x;
     position.y += change.y;
 
-    const outsideX = position.x < 0 || position.x > canvas.element.width;
-    const outsideY = position.y < 0 || position.y > canvas.element.height;
+    const outsideX =
+      position.x + size.width < 0 || position.x > canvas.element.width;
+    const outsideY =
+      position.y + size.height < 0 || position.y > canvas.element.height;
     const isOutsideScreen = outsideX || outsideY;
 
     if (isOutsideScreen) {
