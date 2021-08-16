efe069a7e9a3e2575ff42004fe459b1caf88ce30
Bruno Konrad <bruno.skon@gmail.com>
2021-08-16 13:29:18 +0200

    Stop game when user leaves website's tab

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,11 @@ import { Game } from "./code/Game";
 const game = new Game();
 
 game.start();
+
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "hidden") {
+    game.stop();
+  } else {
+    game.start();
+  }
+});
